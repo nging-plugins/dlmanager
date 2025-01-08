998d78eb227728dbecfc8f7a1939554bc3c688fe
Hank Shen <swh@admpub.com>
2025-01-08 18:51:19 +0800

    update

--- application/handler/navigate.go
+++ application/handler/navigate.go
@@ -1,66 +1,69 @@
 package handler
 
-import "github.com/coscms/webcore/library/navigate"
+import (
+	"github.com/coscms/webcore/library/navigate"
+	"github.com/webx-top/echo"
+)
 
 var LeftNavigate = &navigate.Item{
 	Display: true,
-	Name:    `离线下载`,
+	Name:    echo.T(`离线下载`),
 	Action:  `download`,
 	Icon:    `download`,
 	Children: &navigate.List{
 		{
 			Display: true,
-			Name:    `下载管理`,
+			Name:    echo.T(`下载管理`),
 			Action:  `index.html`,
 		},
 		{
 			Display: true,
-			Name:    `文件管理`,
+			Name:    echo.T(`文件管理`),
 			Action:  `file`,
 		},
 		{
 			Display: false,
-			Name:    `总进度信息`,
+			Name:    echo.T(`总进度信息`),
 			Action:  `progress.json`,
 		},
 		{
 			Display: false,
-			Name:    `添加任务`,
+			Name:    echo.T(`添加任务`),
 			Action:  `add_task`,
 		},
 		{
 			Display: false,
-			Name:    `删除任务`,
+			Name:    echo.T(`删除任务`),
 			Action:  `remove_task`,
 		},
 		{
 			Display: false,
-			Name:    `启动任务`,
+			Name:    echo.T(`启动任务`),
 			Action:  `start_task`,
 		},
 		{
 			Display: false,
-			Name:    `停止任务`,
+			Name:    echo.T(`停止任务`),
 			Action:  `stop_task`,
 		},
 		{
 			Display: false,
-			Name:    `重启任务`,
+			Name:    echo.T(`重启任务`),
 			Action:  `restart_task`,
 		},
 		{
 			Display: false,
-			Name:    `启动所有任务`,
+			Name:    echo.T(`启动所有任务`),
 			Action:  `start_all_task`,
 		},
 		{
 			Display: false,
-			Name:    `停止所有任务`,
+			Name:    echo.T(`停止所有任务`),
 			Action:  `stop_all_task`,
 		},
 		{
 			Display: false,
-			Name:    `单个文件进度信息`,
+			Name:    echo.T(`单个文件进度信息`),
 			Action:  `progress/*`,
 		},
 	},
